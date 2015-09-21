b6d316c16ea7414235ebd57a5d71f6963ef35d35
ThornWinters <MichaelJWarner@hotmail.com>
2015-09-21 10:59:18 -0400

    fixed hamburger button

--- client/app/navbar/controller.js
+++ client/app/navbar/controller.js
@@ -37,6 +37,21 @@ angular
 				id : 4,
 			},
 		];
+
+		var hamburgerOpen = false;
+		$scope.hamburgerClicked = function(){
+			if(hamburgerOpen){
+				$($('.navbar-toggle .icon-bar')[0]).removeClass('navbar-hamburger-button-open-top');
+				$($('.navbar-toggle .icon-bar')[1]).removeClass('navbar-hamburger-button-open-mid');
+				$($('.navbar-toggle .icon-bar')[2]).removeClass('navbar-hamburger-button-open-bottom');
+				hamburgerOpen = false;
+			}else{
+				$($('.navbar-toggle .icon-bar')[0]).addClass('navbar-hamburger-button-open-top');
+				$($('.navbar-toggle .icon-bar')[1]).addClass('navbar-hamburger-button-open-mid');
+				$($('.navbar-toggle .icon-bar')[2]).addClass('navbar-hamburger-button-open-bottom');
+				hamburgerOpen = true;
+			}
+		}
 		$scope.clicked = function(id){
 			sessionStorage.setItem('navbarActive', id);
 		}
